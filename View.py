58917a6a5af85dbc8709b4e6ffeeb9138d86fc8b
Wa2si <wassiimgrb@gmail.com>
2025-01-31 22:31:35 +0100

    - J ai ajouté la fonction afficher_obstacle pour pouvoir directement afficher d autre obsatcle elle est encore a completer si on ajoute des obstacles d autre types

--- View.py
+++ View.py
@@ -26,3 +26,15 @@ def afficher_robot(screen, robot):
     ligne_x = int(screen_x + robot.taille_robot * math.cos(direction))
     ligne_y = int(screen_y + robot.taille_robot * math.sin(direction))
     pygame.draw.line(screen, (255, 255, 255), (screen_x, screen_y), (ligne_x, ligne_y), 2)
+
+
+def afficher_obstacles(screen, obstacles):
+    """
+    Affiche tous les obstacles sur l'écran.
+    """
+    for obstacle in obstacles:
+        if obstacle.type_forme == "rectangle":
+            pygame.draw.rect(screen, COLOR_OBSTACLE, (obstacle.position[0], obstacle.position[1],
+                                                       obstacle.dimensions[0], obstacle.dimensions[1]))
+        elif obstacle.type_forme == "cercle":
+            pygame.draw.circle(screen, COLOR_OBSTACLE, obstacle.position, obstacle.dimensions[0])
