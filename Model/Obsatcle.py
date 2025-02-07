d414f2968667772f658cc691694e4fa019ca12ef
Florent <florentsu2003@gmail.com>
2025-02-07 17:45:27 +0100

    Ajout de la sous classe Ligne avec constructeur

--- Model/Obsatcle.py
+++ Model/Obsatcle.py
@@ -60,3 +60,30 @@ class Rectangle(Obstacle):
             if rx <= px <= rx + largeur and ry <= py <= ry + hauteur:
                 return (True)
         return (False)
+    
+class Ligne(Obstacle):
+    def __init__(self, point1, point2, epaisseur=1):
+        """
+        Initialise une ligne avec deux points et une épaisseur.
+        :param point1: Premier point (x, y)
+        :param point2: Deuxième point (x, y)
+        :param epaisseur: Épaisseur de la ligne
+        """
+        self.point1 = point1
+        self.point2 = point2
+        self.epaisseur = epaisseur
+    
+    def detecter_collision(self, robot):
+        """
+        Vérifie si le robot entre en collision avec la ligne.
+        Retourne (collision: bool, arriere: bool, lateral: bool).
+        """
+        x1, y1 = self.point1
+        x2, y2 = self.point2
+        
+        for px, py in robot.points():
+            dist = abs((y2 - y1) * px - (x2 - x1) * py + x2 * y1 - y2 * x1) / math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
+            if dist <= self.epaisseur / 2:
+                return True
+        return False
+    
