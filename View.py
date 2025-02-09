54a0fced38e9b313b005d3246674e53f3a7b3c8d
Wa2si <wassiimgrb@gmail.com>
2025-02-09 15:28:50 +0100

    Suppresion de afficher_robot2 car on en a plus besoins

--- View.py
+++ View.py
@@ -48,27 +48,6 @@ def afficher_robot(screen, robot):
     # Dessiner une ligne indiquant la direction (optionnel, juste pour visualiser la direction)
     pygame.draw.line(screen, (255, 255, 255), (screen_x, screen_y), (int(point3_x), int(point3_y)), 2)
 
-def afficher_robot2(screen, robot):
-    """
-    Dessine le robot sous forme d'un cercle avec une direction.
-    """
-
-    # Obtenir la position et la direction du robot
-    x, y = robot.x,robot.y
-    direction = robot.direction
-
-    # Convertir la position en pixels
-    screen_x = int(x)
-    screen_y = int(y)
-
-    # Dessiner le robot
-    pygame.draw.circle(screen, ROBOT_COLOR, (screen_x, screen_y), robot.taille_robot)
-
-    # Dessiner une ligne indiquant la direction
-    ligne_x = int(screen_x + robot.taille_robot * math.cos(direction))
-    ligne_y = int(screen_y + robot.taille_robot * math.sin(direction))
-    pygame.draw.line(screen, (255, 255, 255), (screen_x, screen_y), (ligne_x, ligne_y), 2)
-
 
 # Définition de la fonction principale avec @singledispatch
 # Cette fonction est appelée si aucun type enregistré ne correspond
