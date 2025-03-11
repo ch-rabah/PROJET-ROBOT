e38b0f2c787796752242087f9dab5e9e792fcdb7
Florent <florentsu2003@gmail.com>
2025-03-12 00:31:16 +0100

    quelques réglages et ajout fonction gerer_collisions

--- src/model/environnement.py
+++ src/model/environnement.py
@@ -1,7 +1,7 @@
 import math
 
 class Environnement:
-    def __init__(self,dimX= None,dimY= None):
+    def __init__(self, dimX=None, dimY=None):
         self.obstacles = []  # Liste d'objets Obstacle stockés dans l'environnement
         self.dimensions_x = dimX  # Largeur totale du monde
         self.dimensions_y = dimY  # Hauteur totale du monde
@@ -34,8 +34,6 @@ class Environnement:
             rx + taille_robot * math.cos(direction + math.pi),
             ry + taille_robot * math.sin(direction + math.pi)
         )
-        
-        
 
         # Dimensions du monde
         min_x, max_x = self.dimensions_x
@@ -43,10 +41,50 @@ class Environnement:
 
         # Vérifier si le robot est en dehors des limites du monde
         for x, y in [point1, point2, point3, (rx, ry)]:
-            if x  < min_x or x  > max_x or y  < min_y or y  > max_y:
-                    return True
+            if x < min_x or x > max_x or y < min_y or y > max_y:
+                return True
         return False
 
+    def gerer_collisions(self, robot):
+        """
+        Gère les collisions avec les obstacles et la sortie de l'environnement.
+        """
+        for obstacle in self.obstacles:
+            collision = obstacle.detecter_collision(robot)
+            if collision:
+                print("Collision détectée!")
+                robot.arreter_robot()
+
+                dx = obstacle.position[0] - robot.x
+                dy = obstacle.position[1] - robot.y
+                angle_robot = robot.direction
+                angle_obstacle = math.atan2(dy, dx)
+                delta_angle = (angle_obstacle - angle_robot + math.pi) % (2 * math.pi) - math.pi
+
+                if -math.pi / 2 <= delta_angle <= math.pi / 2:
+                    robot.appliquer_vitesse_gauche(-5)
+                    robot.appliquer_vitesse_droite(-5)
+                else:
+                    robot.appliquer_vitesse_gauche(5)
+                    robot.appliquer_vitesse_droite(5)
+
+                break
+
+        # Empêcher le robot de sortir des limites de l'environnement
+        min_x, max_x = self.dimensions_x
+        min_y, max_y = self.dimensions_y
+        if robot.x < min_x:
+            robot.x = min_x
+            robot.arreter_robot()
+        elif robot.x > max_x:
+            robot.x = max_x
+            robot.arreter_robot()
+        if robot.y < min_y:
+            robot.y = min_y
+            robot.arreter_robot()
+        elif robot.y > max_y:
+            robot.y = max_y
+            robot.arreter_robot()
 
 
 
