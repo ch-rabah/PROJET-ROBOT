0405bb005a65ab5d01e2030d6290239cbf0b347b
Wa2si <wassiimgrb@gmail.com>
2025-03-12 00:17:31 +0100

    ajout de set_motor_dps dans robotadapter

--- adapter.py
+++ adapter.py
@@ -11,3 +11,15 @@ class RobotAdapter:
         obstacle_detecte, distance = self.robot.capteurdistance()
         return distance if obstacle_detecte else float("inf")
     
+    def set_motor_dps(self, port, dps):
+        """
+        Fixe la vitesse d'un moteur en degrés par seconde (DPS).
+        Conversion : DPS -> vitesse linéaire en mm/s -> application via les méthodes du robot.
+        """
+          # Conversion en mm/s
+
+        if port == RobotAdapter.MOTOR_LEFT:
+            self.robot.appliquer_vitesse_gauche(dps)
+        elif port == RobotAdapter.MOTOR_RIGHT:
+            self.robot.appliquer_vitesse_droite(dps)
+    
