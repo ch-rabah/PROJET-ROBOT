b8259a8b04716f49e6c9e83f4d4a38ae02915a24
Wa2si <wassiimgrb@gmail.com>
2025-03-12 00:16:57 +0100

    ajout de init et get distance dans la classe robot adapter

--- adapter.py
+++ adapter.py
@@ -2,3 +2,12 @@ class RobotAdapter:
     # Constantes pour le contrôle du robot
     MOTOR_LEFT = "LEFT"
     MOTOR_RIGHT = "RIGHT"
+
+    def __init__(self, robot):
+        self.robot = robot
+
+    def get_distance(self):
+        """Retourne la distance à l'obstacle le plus proche."""
+        obstacle_detecte, distance = self.robot.capteurdistance()
+        return distance if obstacle_detecte else float("inf")
+    
