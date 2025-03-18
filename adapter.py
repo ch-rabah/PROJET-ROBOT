7f08e0bda070368d54ef703e97f22243ca60d13a
Wa2si <wassiimgrb@gmail.com>
2025-03-18 17:48:09 +0100

    mise à jour de calculer_distance_parcourue dans la classe RobotAdapterReel

--- adapter.py
+++ adapter.py
@@ -70,9 +70,20 @@ class RobotAdapterReel(RobotAdapter):
 
     def set_speed_right(self, dps):
         self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, dps)
-        
-    def calculer_distance_parcourue(self):
-        pass
+
+    def calculer_distance_parcourue(self, dt):
+        """Calcul basé sur les encodeurs du robot réel."""
+        l_pos_actuelle, r_pos_actuelle = self.robot.get_motor_position()
+
+        distance_gauche = (l_pos_actuelle - self.pos_initiale[0]) * (self.robot.WHEEL_DIAMETER * 3.14159 / 360)
+        distance_droite = (r_pos_actuelle - self.pos_initiale[1]) * (self.robot.WHEEL_DIAMETER * 3.14159 / 360)
+
+        distance_moyenne = (distance_gauche + distance_droite) / 2
+        self.distance_parcourue += distance_moyenne
+
+        self.pos_initiale = (l_pos_actuelle, r_pos_actuelle)  
+        return self.distance_parcourue
+
 
     def get_distance(self):
         return self.robot.get_distance()
