b13946ca614a92b096ac4f30768ff5faa36c7f4a
Rabah <chrabah2004@gmail.com>
2025-03-18 16:10:54 +0100

    ajout de la strategy conditionnel

--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -88,3 +88,41 @@ class StrategyCarre(Strategy):
                 if self.current_strategy_index >= len(self.strategies):
                     return True  # Stratégie terminée
         return False  # Stratégie en cours
+    
+class StrategyConditionnel(Strategy):
+    def __init__(self, robot_adapter, distance_cible, angle_degrees, distance_cote, condition_tourner, condition_avancer, condition_carre):
+        super().__init__(robot_adapter)
+        self.distance_cible = distance_cible
+        self.angle_degrees = angle_degrees
+        self.distance_cote = distance_cote
+        self.condition_tourner = condition_tourner
+        self.condition_avancer = condition_avancer
+        self.condition_carre = condition_carre
+        self.current_strategy = None
+        self.finished = False
+
+    def __call__(self, dt):
+        """Exécute la stratégie en fonction des conditions."""
+        if self.finished:
+            return True  # Stratégie déjà terminée
+
+        if self.current_strategy is None: #"""or self.current_strategy.is_finished()""":
+            if self.condition_tourner():
+                print("Condition tourner remplie, passage en mode tourner")
+                self.current_strategy = StrategyTourner(self.robot_adapter, self.angle_degrees)
+            elif self.condition_avancer():
+                print("Condition avancer remplie, passage en mode avancer")
+                self.current_strategy = StrategyAvancer(self.robot_adapter, self.distance_cible)
+            elif self.condition_carre():
+                print("Condition carre remplie, passage en mode carré")
+                self.current_strategy = StrategyCarre(self.robot_adapter, self.distance_cote)
+            else:
+                print("Aucune condition remplie, arrêt de la stratégie")
+                self.finished = True
+                return True  # Arrêt définitif de la stratégie
+
+        return False  # La stratégie continue tant qu'il y a une action à exécuter
+
+    def is_finished(self):
+        """Retourne True si la stratégie est terminée."""
+        return self.finished #"""or (self.current_strategy is not None and self.current_strategy.is_finished())"""
