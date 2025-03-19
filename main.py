4f412d81a95dae31171569a8a14e00e46e13fb49
Wa2si <wassiimgrb@gmail.com>
2025-03-19 09:42:41 +0100

    Resolution conflit lors du merge

--- main.py
+++ main.py
@@ -4,68 +4,93 @@ from src.view.affichage_Tkinter import SimulationView
 from src.model.robot import Robot
 from src.model.environnement import Environnement
 from src.model.obstacle import Rectangle, Cercle, Ligne, Triangle
-from src.strategy.strategy import StrategyAvancer, StrategyTourner, StrategyCarre
-from adapter import RobotAdapter
+from src.strategy.strategy import StrategyAvancer, StrategyTourner, StrategyCarre, StrategyConditionnelle, StrategySequentielle
+from adapter import RobotAdapterSimulation
 
 
 def main():
     # Initialisation de l'environnement et du robot
     environnement = Environnement((0, 800), (0, 600))
-    
+
     robot = Robot(400, 300, environnement=environnement, direction=0, vitesse_gauche=0, vitesse_droite=0)
     environnement.ajouter_obstacle(Rectangle((100, 100), (200, 50)))
-    #environnement.ajouter_obstacle(Cercle((500, 200), 30))
     environnement.ajouter_obstacle(Triangle((600, 300), (650, 350), (700, 300)))
-    
-    robot_adapter = RobotAdapter(robot)
+
+    robot_adapter = RobotAdapterSimulation(robot)  # Utilisation de l'adaptateur simulation
     simulation = SimulationView(Tk(), environnement, robot)
 
     # Variables de gestion des stratégies
     current_strategy_index = 0
     previous_time = time.time()
     tempsecouler = 0
-    strategies = [
-            StrategyCarre(robot_adapter, 100),
-    ]"""
-    
-    strategies = [
-        StrategyAvancer(robot_adapter, 200),
-        StrategyTourner(robot_adapter, 90),
-        StrategyTourner(robot_adapter, -45),
-        StrategyAvancer(robot_adapter, 200),
-        StrategyTourner(robot_adapter, 360),       
+
+    avancer = StrategyAvancer(robot_adapter)
+    tourner = StrategyTourner(robot_adapter)
+    carre = StrategyCarre(robot_adapter) 
+
+    # Liste des stratégies
+    strategies1 = [
+        (carre, 50),
+        (avancer, 25),
+        (tourner, -90)
+       
     ]
 
-    # Liste des stratégies : plusieurs carrés avec différentes tailles
-    strategies = [
-        StrategyCarre(robot_adapter, 100),  # Premier carré
-        StrategyCarre(robot_adapter, 150),  # Deuxième carré
-        """StrategyCarre(robot_adapter, 200),  # Troisième carré
-        StrategyCarre(robot_adapter, 250),  # Quatrième carré"""
-    ]
-    
+    # Stratégie conditionnelle
+    strategy_conditionnelle = StrategyConditionnelle(
+        robot_adapter,
+        distance_cible=200,
+        angle_degrees=90,
+        distance_cote=40,
+        condition_tourner=False,
+        condition_avancer=True,
+        condition_carre=False,
+    )
+
+    # Création d'une séquence de stratégies
+    strategy_sequence = StrategySequentielle(
+        robot_adapter, 
+        [
+        (StrategyAvancer, 30),
+        (StrategyTourner, 30),
+        (StrategyCarre, 50),
+        ]
+    )
+
     # Boucle principale
     while True:
         current_time = time.time()
         dt = current_time - previous_time
         previous_time = current_time
         tempsecouler += dt
-
+        
         # Exécuter la stratégie actuelle
-        if current_strategy_index < len(strategies):
-            current_strategy = strategies[current_strategy_index]
-            if current_strategy(dt):
-                # Passer à la stratégie suivante si la stratégie actuelle est terminée
+        if current_strategy_index < len(strategies1):
+            current_strategy, param = strategies1[current_strategy_index]
+            current_strategy(param)
+            current_strategy.execute(dt)
+
+            # Vérifier si la stratégie est terminée
+            if current_strategy.est_terminee():
                 current_strategy_index += 1
 
+        # Exécuter la stratégie conditionnelle une fois que les stratégies fixes sont terminées
+        elif not strategy_sequence.est_terminee():
+            print("strategie sequentielle")
+            strategy_sequence.execute(dt)
+        
+        
+        elif not strategy_conditionnelle.est_terminee():
+            print("strategie conditionnelle")
+            strategy_conditionnelle.execute(dt)
+        
+
+        
         # Mettre à jour l'environnement et l'affichage
         environnement.update(robot, dt)
         simulation.mise_a_jour(tempsecouler)
-        
-        # Limiter la vitesse de mise à jour pour simuler 60 FPS
         time.sleep(1 / 60)
 
 
 if __name__ == "__main__":
     main()
-
