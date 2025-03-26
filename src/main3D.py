f9f4ade4ccbb2e7aeaf696a32ee8cf41e54bf516
Florent <florentsu2003@gmail.com>
2025-03-26 10:41:54 +0100

    changement du main pour les obstacles 3D

--- src/main3D.py
+++ src/main3D.py
@@ -1,6 +1,6 @@
 from model.environnement import Environnement
 from model.robot_3D import Robot
-from model.obstacle import Rectangle, Triangle, Ligne, Sphere
+from model.obstacle_3D import Rectangle3D, Triangle3D, Ligne3D, Sphere3D
 from view.affichage_3D import SimulationView3D
 
 
@@ -9,10 +9,11 @@ def main():
     env = Environnement((0, 100), (0, 100))
 
     # Ajouter des obstacles
-    env.ajouter_obstacle(Rectangle((20, 20), (20, 10)))
-    env.ajouter_obstacle(Sphere((60, 60), 5))
-    env.ajouter_obstacle(Ligne((10, 80), (90, 80), largeur=1))
-    env.ajouter_obstacle(Triangle((20, 70), (30, 90), (40, 70)))
+    env.ajouter_obstacle(Rectangle3D((20, 0, 20), (20, 1, 10)))
+    env.ajouter_obstacle(Sphere3D((60, 0, 60), 5))
+    env.ajouter_obstacle(Ligne3D((10, 0, 80), (90, 0, 80), largeur=1))
+    env.ajouter_obstacle(Triangle3D((20, 0, 70), (30, 0, 90), (40, 0, 70)))
+
 
     # Créer le robot
     robot = Robot(x=50, y=50, environnement=env, direction=0)
