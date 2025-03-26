e85f53958bfd479ff564cb62937bb2d17a70f658
Florent <florentsu2003@gmail.com>
2025-03-26 01:51:20 +0100

    ajout d'obstacles dans le main3D

--- src/main3D.py
+++ src/main3D.py
@@ -1,18 +1,25 @@
 from model.environnement import Environnement
 from model.robot_3D import Robot
+from model.obstacle import Rectangle, Triangle, Ligne, Sphere
 from view.affichage_3D import SimulationView3D
 
 
 def main():
-    # Création d’un environnement simple
-    environnement = Environnement((0, 100), (0, 100))
+    # Créer l'environnement
+    env = Environnement((0, 100), (0, 100))
 
-    # Création du robot (même s'il n'est pas encore affiché)
-    robot = Robot(x=0, y=0, environnement=environnement, direction=0)
+    # Ajouter des obstacles
+    env.ajouter_obstacle(Rectangle((20, 20), (20, 10)))
+    env.ajouter_obstacle(Sphere((60, 60), 5))
+    env.ajouter_obstacle(Ligne((10, 80), (90, 80), largeur=1))
+    env.ajouter_obstacle(Triangle((20, 70), (30, 90), (40, 70)))
 
-    # Lancement de la simulation 3D avec VPython
-    simulation = SimulationView3D(environnement, robot)
-    simulation.run()
+    # Créer le robot
+    robot = Robot(x=50, y=50, environnement=env, direction=0)
+
+    # Lancer l'affichage
+    vue = SimulationView3D(env, robot)
+    vue.run()
 
 
 if __name__ == "__main__":
