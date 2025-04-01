d9cdacd52c2a93f0776ed74142278e5e29191a47
Wa2si <wassiimgrb@gmail.com>
2025-04-02 00:26:26 +0200

    modification des obstacles créent

--- src/main3D.py
+++ src/main3D.py
@@ -8,12 +8,14 @@ from view.affichage_3D import SimulationView3D
 
 
 def main():
-    # Initialisation de l'environnement
+    # Créer l'environnement
     env = Environnement((0, 100), (0, 100))
-    env.ajouter_obstacle(Rectangle3D((20, 0, 20), (20, 1, 10)))
-    env.ajouter_obstacle(Sphere3D((60, 0, 60), 5))
-    env.ajouter_obstacle(Ligne3D((10, 0, 80), (90, 0, 80), largeur=1))
-    env.ajouter_obstacle(Triangle3D((20, 0, 70), (30, 0, 90), (40, 0, 70)))
+
+    # Ajouter des obstacles
+    env.ajouter_obstacle(Rectangle((20, 20), (20, 10)))
+    env.ajouter_obstacle(Cercle((60, 60), 5))
+    env.ajouter_obstacle(Ligne((10, 80), (90, 80), largeur=1))
+    env.ajouter_obstacle(Triangle((20, 70), (30, 90), (40, 70)))
 
     # Création du robot simulé
     robot = Robot(x=50, y=50, environnement=env, direction=0)
