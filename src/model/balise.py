bb9eebe3f9084ac516b20dce505775d5f2b6d907
Florent <florentsu2003@gmail.com>
2025-05-04 17:04:14 +0200

    modification pour respecter la balise réelle

--- src/model/balise.py
+++ src/model/balise.py
@@ -1,16 +1,24 @@
-class Balise:
-    def __init__(self, position, couleur, forme='cube', taille=5, hauteur=0):
-        """
-        Initialise une balise colorée.
+from ursina import color
 
-        :param position: tuple (x, y) représentant la position dans l'environnement (2D)
-        :param couleur: couleur de la balise (nom de couleur Ursina, ex: 'red', 'blue', etc.)
-        :param forme: modèle Ursina à utiliser (par défaut 'cube')
+class Balise:
+    def __init__(self, position, couleurs=None, forme='quad', taille=5, hauteur=0, rotation=0):
+        """
+        Balise rectangulaire multicolore (2x2), orientable.
+
+        :param position: tuple (x, y)
+        :param couleurs: liste de 4 couleurs dans l'ordre [haut-gauche, haut-droite, bas-gauche, bas-droite]
+        :param forme: forme (non utilisé ici)
         :param taille: taille de la balise
-        :param hauteur: hauteur d'élévation dans la scène (axe Y)
+        :param hauteur: base Y de la balise
+        :param rotation: angle de rotation autour de Y (en degrés)
         """
         self.position = position
-        self.couleur = couleur
         self.forme = forme
         self.taille = taille
         self.hauteur = hauteur
+        self.rotation = rotation
+
+        if couleurs is None:
+            self.couleurs = [color.yellow, color.green, color.red, color.blue]
+        else:
+            self.couleurs = couleurs[:4] + [color.white] * (4 - len(couleurs))
