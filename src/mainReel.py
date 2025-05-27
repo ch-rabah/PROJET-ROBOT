bb127d4ac77b25c642891b693db153c1940fc380
Wa2si <wassiimgrb@gmail.com>
2025-05-27 21:37:38 +0200

    modif de initialiser simu en mettant dans les deux main le meme noms

--- src/mainReel.py
+++ src/mainReel.py
@@ -4,7 +4,7 @@ from FWSFR.adapter.adapter import RobotAdapterReel
 from FWSFR.strategy import verif_sequence, initialiser_strategies
 from FWSFR.view import mise_a_jour_simulation, mettre_a_jour_temps
 
-def initialiser_reel():
+def initialiser_simulation():
     robot_adapter = RobotAdapterReel(Robot2IN013())
     sequence = initialiser_strategies(robot_adapter,3,2,10)
     previous_time = time.time()
@@ -14,7 +14,7 @@ def initialiser_reel():
 
 def main():
 
-    sequence, previous_time, elapsed_time = initialiser_reel()
+    sequence, previous_time, elapsed_time = initialiser_simulation()
 
 
     while True:
